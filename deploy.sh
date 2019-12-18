8a8a233409399b337b1f5124cc0e43ced9faf46c
John McCann Cunniff Jr <johncunniff1248@gmail.com>
2019-12-18 01:34:13 -0500

    init

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+check_deps() {
+    local DIRS
+    DIRS=(
+        .data/opengrok-src
+        .data/opengrok-etc
+        .data/opengrok-data
+    )
+
+    for i in "${DIRS[@]}"; do
+        mkdir -p ${i};
+    done
+
+    if ! docker network ls | grep 'traefik-proxy' &> /dev/null; then
+        docker network create traefik-proxy
+    fi
+}
+
+bring_up_service() {
+    docker-compose up -d --force-recreate
+}
+
+main() {
+    check_deps
+    bring_up_service
+}
+
+main
