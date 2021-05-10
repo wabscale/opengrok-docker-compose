53ce6558426bdbf189181867b11898ae05022fac
John McCann Cunniff Jr <johncunniff1248@gmail.com>
2021-05-10 09:18:34 -0400

    update reindex

--- reindex.sh
+++ reindex.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-if ! docker-compose ps | grep 'Up' &> /dev/null; then
-    echo
-    echo '  start the opengrok container plz'
-    echo
-fi
+#if ! docker-compose ps | grep 'Up' &> /dev/null; then
+#    echo
+#    echo '  start the opengrok container plz'
+#    echo
+#fi
 
-docker-compose exec opengrok /scripts/index.sh
+docker-compose -f ../docker-compose.yml exec opengrok /scripts/index.sh
